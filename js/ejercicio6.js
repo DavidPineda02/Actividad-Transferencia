50991e06e194b9ccb0f556318be768fe5d7c29c7
DavidPineda02 <davxpa.02@gmail.com>
2025-09-29 17:41:58 -0500

    Se realizo el Ejercicio 6, Conversion de Minutos a Horas y Minutos

---
+++ js/ejercicio6.js
@@ -0,0 +1,14 @@
+// 6. Conversión de minutos a horas y minutos
+// • Solicita un número de minutos.
+// • Convierte a formato: “X horas y Y minutos”.
+
+// Solicita un número de minutos y se convierte a número
+let totalMins = parseInt(prompt("Ingrese la Cantidad de Minutos:"));
+
+// Calcula las horas y los minutos restantes
+let horas = totalMins / 60;
+let mins = totalMins % 60;
+
+// Muestra el resultado en la consola y en una alerta
+console.log(`${totalMins} minutos son: ${horas} horas y ${mins} minutos`);
+alert(`${totalMins} minutos son: ${horas} horas y ${mins} minutos`);
